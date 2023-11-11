1a2e72a9852144e922be47456c4a4c9b58daf99d
Alan Obregon <alanobregonn1@gmail.com>
2023-11-11 12:50:28 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const days = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wen",
+  "Thu",
+  "Fry",
+  "Sat"
+]
+
+function App() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setDate(new Date());
+  }
+
+  return (
+    <>
+      <div className="container">
+        <ul className="days">
+          { 
+            days.map((day, i) => 
+              <li 
+                key={day} 
+                className={
+                  i === date.getDay() 
+                    ? "active" 
+                    : ""
+                  }
+              >
+                {day}
+              </li>
+            )
+          }
+        </ul>
+        <h1 className="time">
+          { 
+            date.toLocaleTimeString(
+              [], 
+              { hour: "2-digit", minute: "2-digit"}
+            )
+          }
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
